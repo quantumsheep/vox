8bba488a6b3b4b623c2526dc496e32c68747576f
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-05 19:47:32 +0100

    Add intialization function

---
+++ include/vox/vox.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <vox/bindings.hpp>
+#include <vox/gl.hpp>
+
+#include <vox/environment.hpp>
+
+#include <vox/ui/bindings.hpp>
+
+#include <vox/ui/window.hpp>
+
+#include <cstdio>
+
+namespace Vox
+{
+Vox::Environment *Init(int width, int height, const char *title, bool debug = false, std::size_t antialiasing = 4, Vox::Monitor *monitor = nullptr, Vox::Window *share = nullptr);
+} // namespace Vox
---
+++ src/vox.cpp
@@ -0,0 +1,52 @@
+#include <vox/vox.hpp>
+
+static void glfw_error_callback(int error, const char *description)
+{
+    fprintf(stderr, "GLFW error %d: %s\n", error, description);
+}
+
+Vox::Environment *Vox::Init(int width, int height, const char *title, bool debug, std::size_t antialiasing, Vox::Monitor *monitor, Vox::Window *share)
+{
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+    {
+        return nullptr;
+    }
+
+    glfwWindowHint(GLFW_SAMPLES, antialiasing);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, debug ? GL_TRUE : GL_FALSE);
+    glfwWindowHint(GLFW_SRGB_CAPABLE, GL_TRUE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow *window = glfwCreateWindow(width, height, title, monitor, share);
+    if (window == nullptr)
+        return nullptr;
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+
+    if (gladLoadGL() == 0)
+    {
+        fprintf(stderr, "Failed to load GLAD!\n");
+        return nullptr;
+    }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO &io = ImGui::GetIO();
+
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 410");
+
+    return new Vox::Environment(window, io);
+}
