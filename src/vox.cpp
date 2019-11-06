3d17638da2a66b78b595bb6306867a5f6d95e6e6
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-06 01:18:01 +0100

    Fix face culling configuration

--- src/vox.cpp
+++ src/vox.cpp
@@ -42,7 +42,10 @@ std::unique_ptr<Vox::Environment> Vox::Init(int width, int height, const char *t
 
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_MULTISAMPLE);
+
     glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+    glFrontFace(GL_CCW);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
