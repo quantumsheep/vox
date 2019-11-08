64fc2d4d63455a4f5dab208dbe6caab2d9786974
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-08 02:16:08 +0100

    Add cursor mode definers

--- include/vox/cursor.hpp
+++ include/vox/cursor.hpp
@@ -1,9 +1,33 @@
 #pragma once
 
 #include <memory>
+#include <vox/bindings.hpp>
 #include <vox/environment.hpp>
 
 namespace Vox::Cursor
 {
 glm::vec2 GetPosition(std::unique_ptr<Vox::Environment> &env);
+
+void SetMode(std::unique_ptr<Vox::Environment> &env, Vox::CursorMode mode);
+void Stuck(std::unique_ptr<Vox::Environment> &env);
+
+inline void Show(std::unique_ptr<Vox::Environment> &env)
+{
+    return SetMode(env, Vox::CursorMode::Normal);
+}
+
+inline void Free(std::unique_ptr<Vox::Environment> &env)
+{
+    return Show(env);
+}
+
+inline void Hide(std::unique_ptr<Vox::Environment> &env)
+{
+    return SetMode(env, Vox::CursorMode::Hidden);
+}
+
+inline void Disable(std::unique_ptr<Vox::Environment> &env)
+{
+    return SetMode(env, Vox::CursorMode::Normal);
+}
 } // namespace Vox::Cursor
--- src/cursor.cpp
+++ src/cursor.cpp
@@ -7,3 +7,14 @@ glm::vec2 Vox::Cursor::GetPosition(std::unique_ptr<Vox::Environment> &env)
 
     return pos;
 }
+
+void Vox::Cursor::SetMode(std::unique_ptr<Vox::Environment> &env, Vox::CursorMode mode)
+{
+    glfwSetInputMode(env->window, GLFW_CURSOR, static_cast<int>(mode));
+}
+
+void Vox::Cursor::Stuck(std::unique_ptr<Vox::Environment> &env)
+{
+    SetMode(env, Vox::CursorMode::Disabled);
+    glfwSetCursorPos(env->window, 0, 0);
+}
