a941de32081201c7f8242bf9932ca0ad417c7287
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-08 01:33:56 +0100

    Fix quaternion camera trickery

--- src/camera.cpp
+++ src/camera.cpp
@@ -2,12 +2,19 @@
 
 glm::mat4 Vox::Camera::view() const
 {
-    glm::quat rotation_matrix(glm::vec3(
-        glm::radians(this->rotation.x),
-        glm::radians(this->rotation.y),
-        glm::radians(this->rotation.z)));
+    glm::quat pitch = glm::angleAxis(glm::radians(this->rotation.y), glm::vec3(1.0f, 0.0f, 0.0f));
+    glm::quat yaw = glm::angleAxis(glm::radians(this->rotation.x + 180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    glm::quat roll = glm::angleAxis(glm::radians(this->rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
 
-    return glm::mat4_cast(rotation_matrix) * glm::translate(glm::mat4(1.0f), this->position);
+    glm::quat orientation = pitch * yaw * roll;
+    orientation = glm::normalize(orientation);
+
+    glm::mat4 rotate = glm::mat4_cast(orientation);
+
+    glm::mat4 translate = glm::mat4(1.0f);
+    translate = glm::translate(translate, -this->position);
+
+    return rotate * translate;
 }
 
 glm::mat4 Vox::Camera::projection() const
