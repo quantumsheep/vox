5a7eeb1d85ad0ebb0eb654550b9d031423ff920d
quantumsheep <nathanael.dmc@outlook.fr>
2019-11-09 00:11:53 +0100

    Rework Object class to be used with models

--- include/vox/object.hpp
+++ include/vox/object.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vox/gl.hpp>
+#include <vox/model.hpp>
 #include <vox/vertex.hpp>
 
 #include <vector>
@@ -14,18 +15,14 @@ public:
     glm::vec3 rotation;
     glm::vec3 scale;
 
-    std::vector<Vertex> vertices;
+    Vox::Model model;
 
-    unsigned int vbo;
-    unsigned int vao;
-    unsigned int ebo;
-
-    Object(std::vector<Vertex> vertices,
+    Object(Vox::Model model,
            glm::vec3 position = glm::vec3(0.0f),
            glm::vec3 rotation = glm::vec3(0.0f),
            glm::vec3 scale = glm::vec3(1.0f));
 
-    virtual glm::mat4 model();
+    virtual glm::mat4 model_matrix();
     virtual void draw();
 };
 } // namespace Vox
--- src/object.cpp
+++ src/object.cpp
@@ -1,6 +1,6 @@
 #include <vox/object.hpp>
 
-glm::mat4 Vox::Object::model()
+glm::mat4 Vox::Object::model_matrix()
 {
     glm::mat4 model = glm::mat4(1.0f);
     model = glm::translate(model, this->position);
@@ -12,44 +12,15 @@ glm::mat4 Vox::Object::model()
     return model;
 }
 
-Vox::Object::Object(std::vector<Vertex> vertices_, glm::vec3 position_, glm::vec3 rotation_, glm::vec3 scale_)
-    : vertices(vertices_),
+Vox::Object::Object(Vox::Model model_, glm::vec3 position_, glm::vec3 rotation_, glm::vec3 scale_)
+    : model(model_),
       position(position_),
       rotation(rotation_),
       scale(scale_)
 {
-    glGenVertexArrays(1, &this->vao);
-    glGenBuffers(1, &this->vbo);
-    // glGenBuffers(1, &this->ebo);
-
-    glBindVertexArray(this->vao);
-
-    glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
-
-    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->ebo);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
-
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)0);
-
-    glEnableVertexAttribArray(1);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, normal));
-
-    glEnableVertexAttribArray(2);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, texture_coordinates));
-
-    glEnableVertexAttribArray(3);
-    glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, tangent));
-
-    glEnableVertexAttribArray(4);
-    glVertexAttribPointer(4, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, bitangent));
-
-    glBindVertexArray(0);
 }
 
 void Vox::Object::draw()
 {
-    glBindVertexArray(this->vao);
-    glDrawArrays(GL_TRIANGLES, 0, this->vertices.size());
+    this->model.draw();
 }
